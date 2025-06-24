19634558630a0ce39777953377ed911213e9c60a
Aurelien-Dre <adrevet@insa-rennes.fr>
2025-06-24 13:30:22 +0200

    simplification for buffer overrun comprehension

--- README.md
+++ README.md
@@ -4,8 +4,7 @@
 My project is to read a DMX frame using the embassy crate and a the USART module of my STM32F072RB
 
 ## Bug
-Currently, the reception is interrupted during DMX frame transmission, so I hear your proposals for improving my code.
-I also limit the logging level by running the code with ``DEFMT_LOG=warn, cargo run --release``
+Currently, I try to run my programm with ``DEFMT_LOG=warn, cargo run --release`` to read some UART frames but the buffer init cut my process and lose some datas 
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod fmt;
 #[cfg(feature = "defmt")]
 use {defmt_rtt as _, panic_probe as _};
 
+use defmt::info;
 //------------Imports-----------
 use embassy_executor::Spawner;
 use embassy_stm32::{
@@ -13,6 +14,7 @@ use embassy_stm32::{
     peripherals::{self},
     usart::{self, BufferedUart, Config, StopBits},
 };
+use embassy_time::Timer;
 use embedded_io_async::{Read, Write};
 //------------Imports-----------
 
@@ -27,15 +29,13 @@ async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
     //------------USART initialisation-----------
+    let mut tx_buffer = [0u8; 50];
+    let mut rx_buffer = [0u8; 15];
+
     let mut config = Config::default();
-    config.baudrate = 250000; //Baudrate for DMX512 reading
-    config.stop_bits = StopBits::STOP2; //Number of stop bits for DMX512 reading
-
-    let mut tx_buffer = [0u8; 513]; //Buffer for rx_DMA
-    let mut rx_buffer = [0u8; 513]; //Buffer for tx_DMA
-
+    config.baudrate = 250000;
+    config.stop_bits = StopBits::STOP2;
     let mut serial = BufferedUart::new_with_de(
-        //Buffered Uart ready to bidirectionnal communication with DMX512 protocol
         p.USART1,
         Irqs,
         p.PA10,
@@ -49,11 +49,21 @@ async fn main(_spawner: Spawner) {
     //------------UART initialisation-----------
 
     //------------Variables-----------
-    let mut buffer_dmx: [u8; 513] = [10; 513]; //Buffer to store DMX512 frame
-                                               //------------Variables-----------
+    let buffer_tx_first: [u8; 10] = [1; 10]; //Buffer to store DMX512 frame
+    let buffer_tx_second: [u8; 10] = [2; 10]; //Buffer to store DMX512 frame
+    let mut buffer_rx: [u8; 10] = [0; 10]; //Buffer to store DMX512 frame
+                                           //------------Variables-----------
 
-    loop {
-        let _ = serial.read(&mut buffer_dmx).await; //Read until Mark Before Break (symbolised by a Framing error)
-        let _ = serial.write(&buffer_dmx[1..2]).await; //Write a byte on the serial port to check on logical analyzed if the read was correctly done
-    }
+    //first read cycle
+    let _ = serial.write(&buffer_tx_first).await;
+    let a = serial.read(&mut buffer_rx).await; //fill the 10 first slots of the DMA
+
+    //second read cycle
+    let _ = serial.write(&buffer_tx_second).await;
+    let b = serial.read(&mut buffer_rx).await; //is blocked by the maximal size of the dma and only return 5 numbrers form the second buffer
+
+    //print the buffer_rx for debug
+    let _ = serial.write(&buffer_rx).await;
+    info!("{} and {} bits have been readen", a.unwrap(), b.unwrap());
+    loop {}
 }
